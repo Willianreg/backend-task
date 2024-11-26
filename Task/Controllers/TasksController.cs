51fd43570901956c622410ce37aae3141b77763e
Aluno <willianreginaldo2@gmail.com>
2024-11-25 21:15:36 -0300

    First Commit

---
+++ Task/Controllers/TasksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task.Models;
+
+namespace Task.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private static List<ModelTask> modelTasks = new List<ModelTask>();
+
+        [HttpGet]
+        public ActionResult<List<ModelTask>> SearchTasks()
+        {
+            return Ok(modelTasks);
+        }
+    }
+}
+
---
+++ Task/Models/ModelTask.cs
@@ -0,0 +1,8 @@
+﻿namespace Task.Models
+{
+    public class ModelTask
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty ;
+    }
+}
