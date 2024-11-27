9c42bad5bab7cba22cad45dbfdd3a5fbfe535c82
Aluno <willianreginaldo2@gmail.com>
2024-11-26 21:55:43 -0300

    BackEnd

--- Task/Controllers/TasksController.cs
+++ Task/Controllers/TasksController.cs
@@ -15,6 +15,31 @@ namespace Task.Controllers
         {
             return Ok(modelTasks);
         }
+        [HttpPost]
+        public ActionResult<List<ModelTask>>
+     AddTask(ModelTask novo)
+        {
+            if (novo.Id == 0 && modelTasks.Count > 0)
+                novo.Id = modelTasks[modelTasks.Count - 1].Id + 1;
+
+            modelTasks.Add(novo);
+            return Ok(modelTasks);
+        }
+
+        [HttpDelete("{id}")]
+
+        public ActionResult<List<ModelTask>>
+            DeleteTask(int id)
+        {
+            var personagem = modelTasks.Find(x => x.Id == id);
+
+            if (personagem is null)
+                return NotFound("Task não enconctrada!");
+
+            modelTasks.Remove(personagem);
+
+            return Ok("Task excluída com sucesso");
+        }
     }
 }
 
